4aa410c1d17062effd4dc2b568d1b9fe802eecb3
Mohamed Ahmed <ma07041705@gmail.com>
2024-03-23 17:46:34 +0300

    Using the Array Find Method

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningYear = record.find(entry => entry.result === 'W');
+    return winningYear ? winningYear.year : undefined;
+  }
+  
